f65b716c8ce1231b7c101884e517c836c7625e98
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-11-18 12:04:30 +0330

    engagement

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,5 @@
-import ananasImage from "../images/ananas.jpg";
 import rewardImage from "../images/reward.jpg";
 import welcomeImage from "../images/welcome.jpg";
-import sexImage from "../images/sex.jpg";
-import downTownImage from "../images/downtown.jpg";
-import valentineImage from "../images/valentine.jpg";
-import behindImage from "../images/behind.jpg";
-import hookImage from "../images/hook.jpg";
 
 export const getChallenges = (stage) => {
   return challenges[stage]
@@ -13,14 +7,13 @@ export const getChallenges = (stage) => {
 
 export const getReward = () => {
   return {
-    text: `Happy Anniversary my love.
-    lets have a 7 days trip to Crystal Waterworld Resort & Spa in Antalya.`,
+    text: `Happy engagement day my love`,
     image: rewardImage
   }
 }
 export const getWelcome = () => {
   return {
-    text: `Here are some challenges that you should do in order to find your gift (you already know whats your gift)
+    text: `Here are some challenges that you should do in order to find your gift
      there are two types of challenges, the ones which you should answer the question and the ones which you should find a letter based on the text you see and type it in th text box blow.
      Good Luck`,
     image: welcomeImage
@@ -35,84 +28,125 @@ const challenges = [{
   answers: ['mahshid'],
   firstQuestion: true
 }, {
-  question: 'window on your right',
-  desc: '2 letters',
-  hint: 'in my room there is a ticket on the window type what you see on that ticket',
-  answers: ['hi'],
+  question: 'what was the song we listened the most in our firt vacation',
+  desc: '6 letter',
+  hint: 'from me',
+  answers: ['saveme'],
 }, {
-  question: 'Day of the week that we went on our first date',
-  desc: '7 letters',
-  hint: 'shame on you, it was the fourth day',
-  answers: ['tuesday'],
+  question: 'what is your nickname for me ? ',
+  desc: '8 letters',
+  hint: 'you know it :D',
+  answers: ['gholombe'],
 
 }, {
-  question: 'what do i call you in this picture (finglish)',
+  question: 'our first hotel ?',
   desc: '6 letters',
-  hint: 'its a fruit',
-  image: ananasImage,
-  answers: ['ananas', 'aananas'],
-
-}, {
-  question: 'you brought light in to my house, find it in that',
-  desc: '5 letters',
-  hint: 'some thing you bought for me',
-  answers: ['jooon'],
+  hint: '',
+  answers: ['marina'],
 
 },{
-  question: 'what were we doing before this picture',
-  desc: '3 letters',
-  hint: 'you know what were we doing :D',
-  image: sexImage,
-  answers: ['sex'],
+  question: 'what did you get me for my second birthday ?',
+  desc: '7 letters',
+  hint: 'what hint are you looking for ? did you get me anything ?',
+  answers: ['nothing'],
 
 }, {
-  question: 'lets play Eldorado and the have some ....',
+  question: 'where was our first outdoor sex?',
   desc: '4 letters',
-  hint: 'there is a ticket on the game',
-  answers: ['beer'],
+  hint: 'shahrak',
+  answers: ['omid', 'shahrakomid'],
 
-}, {
-  question: 'when is this ?',
+},{
+  question: 'what is my favorite position?\n',
   desc: '9 letters',
-  hint: 'love day',
-  image: valentineImage,
-  answers: ['valentine', 'valentineday'],
+  hint: 'your legs will rich the skies',
+  answers: ['forghooni'],
 
 }, {
-  question: 'what does the smallest plant say ?',
-  desc: '4 letters',
-  hint: 'go to Terrace',
-  answers: ['hug'],
+  question: 'where did I kiss you for the first time ? \n',
+  desc: '3 letters',
+  hint: 'it was in my car in front of ....',
+  answers: ['ava'],
+
+}, {
+  question: 'what was the first movie that we almost watch together ?',
+  desc: '12 letters',
+  hint: 'christopher nolan',
+  answers: ['interstellar'],
+
+}, {
+  question: 'our last hotel ?',
+  desc: '8 letters',
+  hint: '',
+  answers: ['granada'],
+
+}, {
+  question: 'what was our seat number in airplane coming back to tehran \n',
+  desc: 'you can find the ticket between the teas',
+  hint: '',
+  answers: ['12c'],
 
 },{
-  question: 'whats behind this photo? ',
-  desc: '2 letters',
-  hint: 'not this photo the physical photo',
-  image: behindImage,
-  answers: ['hm'],
+  question: 'how many points did you get in only game you won ',
+  desc: 'I have thrown it out',
+  hint: 'it was garbage, two middle circle are ten points and each circuit decrease by one point',
+  answers: ['46','47','48','49','45','44'],
 
 }, {
-  question: 'find it in the most useful item in the kitchen',
-  desc: '4 letters',
-  hint: 'it does everything',
-  answers: ['boos']
+  question: 'fill the blanks “love you from enghelab complex ------ call me ----”',
+  desc: 'find the letter inside of a drawer',
+  hint: '',
+  answers: ['ashghalibabei']
 }, {
-  question: 'where did we celebrate your birthday ?',
-  desc: '4 letters',
-  hint: 'in Kish, reverse of kooh',
-  image: hookImage,
-  answers: ['hook', 'hooklounge'],
+  question: 'im a note inside a big red container and there are 3 ways to get to me',
+  desc: 'find me inside that container',
+  hint: 'you will need a key to get inside it',
+  answers: ['letsgo'],
 
 }, {
-  question: 'where is this ? ',
-  desc: '8 letters',
-  hint: 'very close to balcafe',
-  answers: ['downtown'],
-  image: downTownImage,
+  question: 'waiting for you in india',
+  desc: '5 letters',
+  hint: '',
+  answers: ['hello'],
 }, {
-  question: 'find a box between glasses and see what does it say ?  ',
+  question: 'find me inside of a mountain climbing tools\n',
+  desc: '4 letters',
+  hint: 'its so high',
+  answers: ['beta']
+}, {
+  question: 'there are many plastics around me but im very special, keep the content it will be needed later\n',
+  desc: 'its written on the content',
+  hint: 'the plastic reminds you of a very good trip',
+  answers: ['fire'],
+}, {
+  question: 'what was the birthday girl name in Antaliya\n',
   desc: '8 letters',
-  hint: 'look at the bottom of the box',
-  answers: ['loveyou'],
+  hint: '',
+  answers: ['fereshte']
+},{
+  question: 'tha captain name ?',
+  desc: '7 letters',
+  hint: 'tv serie',
+  answers: ['reymond']
+},{
+  question: 'how many days has it been',
+  desc: '',
+  hint: 'last digit is 7',
+  answers: ['427']
+},{
+  question: 'im behind a shy girl \n',
+  desc: '4 letters',
+  hint: 'your last present to me',
+  answers: ['gooz']
+},{
+  question: 'meet me in the park',
+  desc: '5 letters',
+  hint: '',
+  answers: ['light']
+}, {
+  question: 'light it up see the question and answer me ',
+  desc: '',
+  hint: '',
+  answers: ['yes'],
   lastQuestion: true
 }]
